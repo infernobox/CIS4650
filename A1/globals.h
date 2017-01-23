2fd902fae45c2c789f857a805ef0ad5b47378d1e
Vanessa White <whitevanessa14@gmail.com>
2017-01-23 13:49:51 -0500

    Adding news.l, Makefile and globals.h

---
+++ A1/globals.h
@@ -0,0 +1,16 @@
+/**
+ * Author: Vanessa White
+ * Class: CIS*4650 Compilers
+ * Purpose: Global types and variables for the news article written in SGML
+ * Date: January 2017
+*/
+
+#ifndef _GLOBALS_H_
+#define _GLOBALS_H_
+
+using namespace std;
+
+#include <cstdio>
+
+enum Tags { DOC };
+#endif
---
+++ A1/scanner.cpp
@@ -0,0 +1,14 @@
+/**
+ * Author: Vanessa White
+ * Class: CIS*4650 Compilers
+ * Purpose: Scanner for news article written in SGML
+ * Date: January 2017
+*/
+
+#include "globals.h"
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
