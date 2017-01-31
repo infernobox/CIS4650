032788191a98b8193a382ad22c6bc253ce7e4b22
Vanessa White <whitevanessa14@gmail.com>
2017-01-30 22:55:54 -0500

    Trying to filter still...while loop is infinite

--- A1/util.cpp
+++ A1/util.cpp
@@ -20,64 +20,76 @@ array<string, 7> relevantTags = { "DOC", "TEXT", "P", "DOCNO", "DATE", "HEADLINE
 void printToken(FILE * f, enum Filters token, struct TokenStack &tokenStack, struct GlobalStack &gs) {
 
 	bool relevant = false;
+	bool poppedMatch = false;
 	/* recognizing tokens sent by the FLEX parser */
 	switch(token)
 	{
 		case ENDFILE:
 			break;
 		case OPENTAG:
-			relevant = checkRelevancy(tokenStack.value);
+			/* Check if it is a relevant tag, if not do not output it */
+			relevant = checkRelevancy(tokenStack.value); 
 
 			if(relevant)
 			{
-				cout << "*OPEN-" << tokenStack.value;
-				normalizedStack.push(tokenStack.value);
+				cout << "*OPEN-" << tokenStack.value << endl;
 			}
-			//cout << "*s " << gs.stck.top() << endl;
+			normalizedStack.push(tokenStack.value);
 			break;
 		case CLOSETAG:
+			/* Check if it is a relevant tag, if not do not output it */
 			relevant = checkRelevancy(tokenStack.value);
-
+			cout << "CLOSE FOUND " << tokenStack.value << endl;
+			cout << normalizedStack.top() << endl;
 			if(relevant)
 			{
-				cout << "*CLOSE-" << tokenStack.value << endl;
-
-				if(normalizedStack.top().compare(tokenStack.value) == 0)
-				{
-					//cout << "MATCHING OPEN & CLOSE TAG" << endl;
-					normalizedStack.pop();
-				}
-				else
-				{
-					/* Have to add them to some sort of error check to display at the end of input */
-					noMatch.push(normalizedStack.top());
-				//	cout << "error: no matching open-tag for </" << normalizedStack.top() << ">" << endl;
-				}
+				/* Keep looping until we find the tag that we need to to match to open tag
+					might have the case where the open tag does not have a matching close tag
+					then it halts output on the correct close-tag
+				*/
+				//while(!poppedMatch) 
+				//{
+					if(normalizedStack.top().compare(tokenStack.value) == 0)
+					{
+						//cout << "MATCHING OPEN & CLOSE TAG" << endl;
+						cout << "*CLOSE-" << tokenStack.value << endl;
+						normalizedStack.pop();
+						poppedMatch = true;
+					}
+					else
+					{
+						/* Have to add them to some sort of error check to display at the end of input */
+						if(checkRelevancy(normalizedStack.top()))
+						{
+							noMatch.push(normalizedStack.top());
+						}
+						else
+						{
+							normalizedStack.pop();
+						}
+					}
+				//}
 			}
-			
-			//cout << "*s " << gs.stck.top() << endl;
+			else
+			{
+				normalizedStack.pop();
+			}
 			break;
 		case WORD:
-			//cout << tokenStack.value;
+			relevant = checkRelevancy(normalizedStack.top());
+
 			if(relevant)
 			{
-				cout << "WORD(" << tokenStack.value.c_str() << ")" << endl;	
+				cout << "*WORD(" << tokenStack.value.c_str() << ")" << endl;	
 			}
 			break;
 	}
 
-/*	while(!noMatch.empty())
+	while(!noMatch.empty())
 	{
-		cout << "error: no matching open-tag for </" << noMatch.top() << ">" << endl;
+		cout << "error: no matching closing-tag for <" << noMatch.top() << ">" << endl;
 		noMatch.pop();
 	}
-
-	while(!normalizedStack.empty())
-	{
-		cout << "error: no matching close-tag for <" << normalizedStack.top() << ">" << endl;
-		normalizedStack.pop();
-	}
-*/
 }
 
 bool checkRelevancy(string token) {
@@ -86,6 +98,7 @@ bool checkRelevancy(string token) {
 	{
 		if(token.compare(relevantTags.at(i)) == 0)
 		{
+			cout << "rele " << relevantTags.at(i) << endl;
 			return true;
 		}
 	}
